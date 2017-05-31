61d78c9803e63e32cd0ade71e397939f03ec1ad0
cphelps987 <cphelps987@gmail.com>
2017-05-31 15:55:55 -0400

    adding html and images

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+//start
+    $('.start').on('click', function()) {
+
+        timer ()
+
+        var correct = 0,
+            incorrect = 0,
+            unanswered = 3 - (correct - incorrect);
+
+    });
+
+
+});

522f7aae7458abf0f6f70ee55cfa4b5144957700
cphelps987 <cphelps987@gmail.com>
2017-06-01 17:42:14 -0400

    adding html, js, add timer, and start with a show/hide

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,44 @@
 $(document).ready(function () {
 
+    console.log("ready!");
+
+
+
 //start
-    $('.start').on('click', function()) {
+    $('#start').on('click', function() {
 
-        timer ()
+        $(function () {
+            $('#question').removeClass('hidden');
+        });
+        $("#start").hide();
 
-        var correct = 0,
-            incorrect = 0,
-            unanswered = 3 - (correct - incorrect);
+       //start btn ends and removed and content
 
+        runTimer();
     });
 
+    var number = 5;
+    var intervalId;
 
-});
+
+    function runTimer() {
+        intervalId = setInterval(decrement, 1000);
+    }
+        function decrement() {
+            number--;
+            $("#time-left").html("<h2>" + number + "</h2>");
+
+            if (number === 0) {
+             stopTimer();
+             }
+    }
+
+    function stopTimer() {
+     clearInterval(intervalId);
+     }
+
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 3 - (correct - incorrect);
+
+}); //end of doc ready
